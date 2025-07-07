45e8ef700ceaac5a3f9934f7db789fab3fa28e0e
Pato1753 <patricio.olivares1753@gmail.com>
2025-07-06 20:33:34 -0400

    mostrar datos tiempo real

--- my-eva3/app/page.tsx
+++ my-eva3/app/page.tsx
@@ -6,39 +6,54 @@ import { Persona } from "./Persona";
 const initialStatePersona:Persona = {
   nombre : "",
   apellido:"",
-  rut:0
+  rut: 0  //solucionar estoooooooooooooooo
 }
 
 export default function Home() {
   const miStorange = window.localStorage
-  const [persona, setpersonaNom] = useState(initialStatePersona)
+  const [personaNom, setpersonaNom] = useState(initialStatePersona)
   const [personaApe, setpersonaApe] = useState(initialStatePersona)
   const [personaRUT, setpersonaRUT] = useState(initialStatePersona)
   const [personas, setpersonas] = useState<Persona[]>([])
   const [eNombre, seteNombre] = useState("")
   const [eApellido, setEApellido] = useState("")
   const [eRUT, setERUR] = useState("")
+
+  const handlePersona = (name:string, value:string)=>{
+    setpersonaNom(
+      {...personaNom,[name] : value }
+    )
+  }
+
   return (
-    <div>
+    <>
       <h1 style={{color:"blue", 
         backgroundColor : "black", 
         fontSize : 40, 
-        textAlign : "center"}}>
-        Lista de estudiante</h1>
-        <div style={{textAlign : "right", marginRight : 20}}>
+        textAlign : "center"}}> Lista de estudiante </h1>
+        <div style={{textAlign : "right", marginRight : 20,marginTop : 20}}>
+          <p>Nombre:  {personaNom.nombre}</p>
+          <p>Apellido: {personaNom.apellido}</p>
+          <p>Rut:  {personaNom.rut}</p>
           <form>
-            <input style={{textAlign : "center"}}
-             type="text" name="nombre" placeholder="Nombre"/>
+            <input style={{textAlign : "center", backgroundColor:'#D3D3D3'}}
+             type="text" name="nombre" placeholder="Nombre"
+             onChange={(e)=>{handlePersona(e.currentTarget.name,e.currentTarget.value)}}/>
             <br />
-            <input type="text" name="apellido" placeholder="Apellido" />
+            <input style={{textAlign : "center",backgroundColor:'#D3D3D3', marginTop:5}}
+            type="text" name="apellido" placeholder="Apellido"
+            onChange={(e)=>{handlePersona(e.currentTarget.name,e.currentTarget.value)}}/>
             <br />
-            <input type= "number" name="run" placeholder="Rut" />
+            <input style={{textAlign : "center",backgroundColor:'#D3D3D3', marginTop:5}}
+            type= "number" name="run" placeholder="Rut"
+            onChange={(e)=>{handlePersona(e.currentTarget.name,e.currentTarget.value)}}/>
             <br />
-            <button id="BRegistro">Soy el boton de registro</button>
+            <button style={{backgroundColor:"grey",border: '2px solid rgb(0, 0, 0)', marginTop:10, marginRight:13}}
+            id="BRegistro">Soy el boton de registro</button>
           </form>
         </div>
 
 
-    </div>
+    </>
     );
 }
