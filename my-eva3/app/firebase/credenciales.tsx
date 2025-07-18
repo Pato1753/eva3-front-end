20486deee2783b8a292fe996b8da9675a070e8b3
Pato1753 <patricio.olivares1753@gmail.com>
2025-07-18 01:28:47 -0400

    Termino de conectividad base de datos
    Se termino de crear los archivos para conectar la base de datos, creando el archivo de credenciales para una mayor seguridad y solucionando pequeños errores de sintaxis.

---
+++ my-eva3/app/firebase/credenciales.tsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB84sxZO0T24Dd6RxwF0qxssFfnh3hWhOk",
+  authDomain: "eva4-frontend.firebaseapp.com",
+  projectId: "eva4-frontend",
+  storageBucket: "eva4-frontend.appspot.com",
+  messagingSenderId: "912536572900",
+  appId: "1:912536572900:web:aff9acc78cae6ab2d82df1",
+  measurementId: "G-S7TN0840T5"
+};
--- my-eva3/app/firebase/firebaseServise.tsx
+++ my-eva3/app/firebase/firebaseServise.tsx
@@ -1,9 +1,10 @@
+import { Persona } from '../Persona';
 import { db } from './firebaseConfig';
 import { collection, addDoc, getDocs, deleteDoc, doc } from 'firebase/firestore';
 
 const estudiantesRef = collection(db, "estudiantes");
 
-export const agregarEstudiante = async (estudiante) => {
+export const agregarEstudiante = async (estudiante:Persona) => {
   await addDoc(estudiantesRef, estudiante);
 };
 
@@ -12,7 +13,7 @@ export const obtenerEstudiantes = async () => {
   return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 };
 
-export const eliminarEstudiante = async (id) => {
+export const eliminarEstudiante = async (id:string) => {
   const estudianteDoc = doc(db, "estudiantes", id);
   await deleteDoc(estudianteDoc);
 };
