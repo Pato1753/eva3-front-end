80f6bea969abfeba9c77f7775b29e234cf5f2a23
Pato1753 <patricio.olivares1753@gmail.com>
2025-07-18 01:19:48 -0400

    Inicio firebase
    Se crea los primeros archivos para implementar una base de datos noSql a react

---
+++ my-eva3/app/firebase/firebaseServise.ts
@@ -0,0 +1,18 @@
+import { db } from './firebaseConfig';
+import { collection, addDoc, getDocs, deleteDoc, doc } from 'firebase/firestore';
+
+const estudiantesRef = collection(db, "estudiantes");
+
+export const agregarEstudiante = async (estudiante) => {
+  await addDoc(estudiantesRef, estudiante);
+};
+
+export const obtenerEstudiantes = async () => {
+  const snapshot = await getDocs(estudiantesRef);
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
+
+export const eliminarEstudiante = async (id) => {
+  const estudianteDoc = doc(db, "estudiantes", id);
+  await deleteDoc(estudianteDoc);
+};
